083e51212c1cb5d85b9aac92261936f0d753f0c8
etiennebacher <etienne.bacher@protonmail.com>
2021-07-17 19:01:51 +0200

    fix function get_cv

--- R/get_cv.R
+++ R/get_cv.R
@@ -4,4 +4,4 @@ download.file("https://raw.githubusercontent.com/etiennebacher/CV_perso/master/c
 # Make the button "CV" open in a new tab
 index_html <- readLines("_site/index.html")
 index_html <- gsub('href="cv.pdf"', 'href="cv.pdf" target = "blank"', index_html)
-cat(index_html, file = "_site/index.html")
+cat(index_html, sep = "\n", file = "_site/index.html")
