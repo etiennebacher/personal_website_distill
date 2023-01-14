266b5551d07bb49b6909bb3c5ab1a5f48b05841b
etiennebacher <etienne.bacher@protonmail.com>
2023-01-14 17:01:44 +0100

    finish post on family meeting

--- _site/R/get_cv.R
+++
@@ -1,7 +0,0 @@
-# Obtain cv.pdf
-download.file("https://raw.githubusercontent.com/etiennebacher/CV_perso/master/cv.pdf", "_site/cv.pdf")
-
-# Make the button "CV" open in a new tab
-index_html <- readLines("_site/index.html")
-index_html <- gsub('href="cv.pdf"', 'href="cv.pdf" target = "blank"', index_html)
-cat(index_html, sep = "\n", file = "_site/index.html")
