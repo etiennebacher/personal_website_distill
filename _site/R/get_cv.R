8783edf20df4aa13b5b1ce9eb6ba410f812bbd4f
etiennebacher <etienne.bacher@protonmail.com>
2023-05-09 14:53:16 +0200

    add toc

--- _site/R/get_cv.R
+++
@@ -1,9 +0,0 @@
-# Obtain cv.pdf
-download.file("https://raw.githubusercontent.com/etiennebacher/CV_perso/master/cv.pdf", "_site/cv.pdf")
-download.file("https://raw.githubusercontent.com/etiennebacher/good-practices/master/good-practices.pdf", "_site/good-practices.pdf")
-
-# Make the button "CV" open in a new tab
-index_html <- readLines("_site/index.html")
-index_html <- gsub('href="cv.pdf"', 'href="cv.pdf" target = "blank"', index_html)
-index_html <- gsub('href="good-practices.pdf"', 'href="good-practices.pdf" target = "blank"', index_html)
-cat(index_html, sep = "\n", file = "_site/index.html")
